446c2cc97d8b828f173aa9999532587bdab18207
hquanvo <vo.hoang.quan2003@gmail.com>
2023-07-03 18:05:56 +0700

    Add ability to draw

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,24 @@
+let isMouseDown = false;
+document.body.onmousedown = () => isMouseDown = true;
+document.body.onmouseup = () => isMouseDown = false;
+
+// Creates the canvas grid that contains size * size grid element
 function createGrid(size) {
     const canvas = document.getElementById("canvas");
     let canvasLength = canvas.offsetHeight;
-    console.log(canvasLength);
     for (let i = 0; i < size * size; i++) {
         let gridElement = document.createElement('div');
         gridElement.classList.add("grid-element");
         gridElement.style.width = (canvasLength / size) + 'px';
         gridElement.style.height = 'auto';
-        gridElement.style.backgroundColor = "white";
-        canvas.appendChild(gridElement);
-        
+        gridElement.addEventListener('mouseover', changeColor)
+        gridElement.addEventListener('mousedown', changeColor)
+        canvas.appendChild(gridElement);    
     }
-    let gridElement = document.querySelector('.grid-element');
-    console.log(gridElement.style.width);
 }
 
-createGrid(16);
+function changeColor(e) {
+    if (!isMouseDown && e.type === 'mouseover') return;
+    e.target.style.backgroundColor = "black";
+}
+createGrid(24);
